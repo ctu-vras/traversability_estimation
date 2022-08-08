0e48c03b9f050735fca68413571b52d9a64da406
RuslanAgishev <agishrus@fel.cvut.cz>
2022-08-08 18:22:36 +0200

    added laser scan class for rellis 3d

--- src/datasets/rellis_3d.py
+++ src/datasets/rellis_3d.py
@@ -271,6 +271,225 @@ class Rellis3D(BaseDataset):
         return image.copy(), mask.copy()
 
 
+class LaserScan:
+    """
+    Class that contains LaserScan with x,y,z,r:
+    https://github.com/PRBonn/semantic-kitti-api/blob/8e75f4d049b787321f68a11753cb5947b1e58e17/auxiliary/laserscan.py
+    """
+    EXTENSIONS_SCAN = ['.bin']
+
+    def __init__(self, project=False, H=64, W=1024, fov_up=3.0, fov_down=-25.0):
+        self.project = project
+        self.proj_H = H
+        self.proj_W = W
+        self.proj_fov_up = fov_up
+        self.proj_fov_down = fov_down
+        self.reset()
+
+    def reset(self):
+        """ Reset scan members. """
+        self.points = np.zeros((0, 3), dtype=np.float32)        # [m, 3]: x, y, z
+        self.remissions = np.zeros((0, 1), dtype=np.float32)    # [m ,1]: remission
+
+        # projected range image - [H,W] range (-1 is no data)
+        self.proj_range = np.full((self.proj_H, self.proj_W), -1,
+                                  dtype=np.float32)
+
+        # unprojected range (list of depths for each point)
+        self.unproj_range = np.zeros((0, 1), dtype=np.float32)
+
+        # projected point cloud xyz - [H,W,3] xyz coord (-1 is no data)
+        self.proj_xyz = np.full((self.proj_H, self.proj_W, 3), -1,
+                                dtype=np.float32)
+
+        # projected remission - [H,W] intensity (-1 is no data)
+        self.proj_remission = np.full((self.proj_H, self.proj_W), -1,
+                                      dtype=np.float32)
+
+        # projected index (for each pixel, what I am in the pointcloud)
+        # [H,W] index (-1 is no data)
+        self.proj_idx = np.full((self.proj_H, self.proj_W), -1,
+                                dtype=np.int32)
+
+        # for each point, where it is in the range image
+        self.proj_x = np.zeros((0, 1), dtype=np.float32)        # [m, 1]: x
+        self.proj_y = np.zeros((0, 1), dtype=np.float32)        # [m, 1]: y
+
+        # mask containing for each pixel, if it contains a point or not
+        self.proj_mask = np.zeros((self.proj_H, self.proj_W),
+                                  dtype=np.int32)       # [H,W] mask
+
+    def size(self):
+        """ Return the size of the point cloud. """
+        return self.points.shape[0]
+
+    def __len__(self):
+        return self.size()
+
+    def open_scan(self, filename):
+        """ Open raw scan and fill in attributes
+        """
+        # reset just in case there was an open structure
+        self.reset()
+
+        # check filename is string
+        if not isinstance(filename, str):
+            raise TypeError("Filename should be string type, "
+                            "but was {type}".format(type=str(type(filename))))
+
+        # check extension is a laserscan
+        if not any(filename.endswith(ext) for ext in self.EXTENSIONS_SCAN):
+            raise RuntimeError("Filename extension is not valid scan file.")
+
+        # if all goes well, open pointcloud
+        scan = np.fromfile(filename, dtype=np.float32)
+        scan = scan.reshape((-1, 4))
+
+        # put in attribute
+        points = scan[:, 0:3]    # get xyz
+        remissions = scan[:, 3]  # get remission
+        self.set_points(points, remissions)
+
+    def set_points(self, points, remissions=None):
+        """ Set scan attributes (instead of opening from file)
+        """
+        # reset just in case there was an open structure
+        self.reset()
+
+        # check scan makes sense
+        if not isinstance(points, np.ndarray):
+            raise TypeError("Scan should be numpy array")
+
+        # check remission makes sense
+        if remissions is not None and not isinstance(remissions, np.ndarray):
+            raise TypeError("Remissions should be numpy array")
+
+        # put in attribute
+        self.points = points    # get xyz
+        if remissions is not None:
+            self.remissions = remissions  # get remission
+        else:
+            self.remissions = np.zeros((points.shape[0]), dtype=np.float32)
+
+        # if projection is wanted, then do it and fill in the structure
+        if self.project:
+            self.do_range_projection()
+
+    def do_range_projection(self):
+        """ Project a pointcloud into a spherical projection image.projection.
+            Function takes no arguments because it can be also called externally
+            if the value of the constructor was not set (in case you change your
+            mind about wanting the projection)
+        """
+        # laser parameters
+        fov_up = self.proj_fov_up / 180.0 * np.pi      # field of view up in rad
+        fov_down = self.proj_fov_down / 180.0 * np.pi  # field of view down in rad
+        fov = abs(fov_down) + abs(fov_up)  # get field of view total in rad
+
+        # get depth of all points
+        depth = np.linalg.norm(self.points, 2, axis=1)
+
+        # get scan components
+        scan_x = self.points[:, 0]
+        scan_y = self.points[:, 1]
+        scan_z = self.points[:, 2]
+
+        # get angles of all points
+        yaw = -np.arctan2(scan_y, scan_x)
+        pitch = np.arcsin(scan_z / (depth + 1e-8))
+
+        # get projections in image coords
+        proj_x = 0.5 * (yaw / np.pi + 1.0)          # in [0.0, 1.0]
+        proj_y = 1.0 - (pitch + abs(fov_down)) / fov        # in [0.0, 1.0]
+
+        # scale to image size using angular resolution
+        proj_x *= self.proj_W                              # in [0.0, W]
+        proj_y *= self.proj_H                              # in [0.0, H]
+
+        # round and clamp for use as index
+        proj_x = np.floor(proj_x)
+        proj_x = np.minimum(self.proj_W - 1, proj_x)
+        proj_x = np.maximum(0, proj_x).astype(np.int32)   # in [0,W-1]
+        self.proj_x = np.copy(proj_x)  # store a copy in orig order
+
+        proj_y = np.floor(proj_y)
+        proj_y = np.minimum(self.proj_H - 1, proj_y)
+        proj_y = np.maximum(0, proj_y).astype(np.int32)   # in [0,H-1]
+        self.proj_y = np.copy(proj_y)  # stope a copy in original order
+
+        # copy of depth in original order
+        self.unproj_range = np.copy(depth)
+
+        # order in decreasing depth
+        indices = np.arange(depth.shape[0])
+        order = np.argsort(depth)[::-1]
+        depth = depth[order]
+        indices = indices[order]
+        points = self.points[order]
+        remission = self.remissions[order]
+        proj_y = proj_y[order]
+        proj_x = proj_x[order]
+
+        # assing to images
+        self.proj_range[proj_y, proj_x] = depth
+        self.proj_xyz[proj_y, proj_x] = points
+        self.proj_remission[proj_y, proj_x] = remission
+        self.proj_idx[proj_y, proj_x] = indices
+        self.proj_mask = (self.proj_idx > 0).astype(np.float32)
+
+
+def laser_scan_demo():
+    import vispy
+    from vispy.scene import visuals, SceneCanvas
+    from matplotlib import pyplot as plt
+
+    def get_mpl_colormap(cmap_name):
+        cmap = plt.get_cmap(cmap_name)
+        # Initialize the matplotlib color map
+        sm = plt.cm.ScalarMappable(cmap=cmap)
+        # Obtain linear color range
+        color_range = sm.to_rgba(np.linspace(0, 1, 256), bytes=True)[:, 2::-1]
+        return color_range.reshape(256, 3).astype(np.float32) / 255.0
+
+    # new canvas prepared for visualizing data
+    canvas = SceneCanvas(keys='interactive', show=True)
+
+    # grid
+    grid = canvas.central_widget.add_grid()
+
+    # laserscan part
+    scan_view = vispy.scene.widgets.ViewBox(border_color='white', parent=canvas.scene)
+    grid.add_widget(scan_view, 0, 0)
+    scan_vis = visuals.Markers()
+    scan_view.camera = 'turntable'
+    scan_view.add(scan_vis)
+    visuals.XYZAxis(parent=scan_view.scene)
+
+    scan = LaserScan(project=True)
+    scan_path = os.path.join(data_dir, 'Rellis_3D', '00000', 'os1_cloud_node_kitti_bin', '002218.bin')
+    scan.open_scan(scan_path)
+
+    # plot scan
+    power = 16
+    # print()
+    range_data = np.copy(scan.unproj_range)
+    # print(range_data.max(), range_data.min())
+    range_data = range_data ** (1 / power)
+    # print(range_data.max(), range_data.min())
+    viridis_range = ((range_data - range_data.min()) /
+                     (range_data.max() - range_data.min()) *
+                     255).astype(np.uint8)
+    viridis_map = get_mpl_colormap("viridis")
+    viridis_colors = viridis_map[viridis_range]
+
+    scan_vis.set_data(scan.points,
+                      face_color=viridis_colors[..., ::-1],
+                      edge_color=viridis_colors[..., ::-1],
+                      size=1)
+
+    vispy.app.run()
+
+
 def semseg_test():
     from datasets.utils import visualize, convert_color
     from hrnet.core.function import convert_label
