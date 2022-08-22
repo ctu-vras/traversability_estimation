2037cebdb9c11407eb030781cbba9840444dfc99
RuslanAgishev <agishrus@fel.cvut.cz>
2022-08-22 15:41:11 +0200

    traversability cloud dataset: colorized point cloud demo

--- src/datasets/traversability_dataset.py
+++ src/datasets/traversability_dataset.py
@@ -83,7 +83,6 @@ class TraversabilityImages(torch.utils.data.Dataset):
 
 
 class TraversabilityClouds:
-
     def __init__(self,
                  path=None,
                  num_samples=None,
@@ -98,7 +97,10 @@ class TraversabilityClouds:
         self.path = path
         self.W = W
         self.H = H
-        self.scan = LaserScan(project=True, H=self.H, W=self.W, fov_up=fov_up, fov_down=fov_down)
+        self.color_map = COLOR_MAP
+        self.scan = SemLaserScan(nclasses=len(LABELS), sem_color_dict=self.color_map,
+                                 project=True, H=self.H, W=self.W, fov_up=fov_up, fov_down=fov_down)
+        self.mask_targets = {val: key for key, val in LABELS.items()}
 
         self.files = self.read_files()
         if num_samples:
@@ -112,7 +114,17 @@ class TraversabilityClouds:
     def __getitem__(self, index):
         cloud_path = self.files[index]
         cloud = np.load(cloud_path, allow_pickle=True)['arr_0'].item()['cloud']
-        return cloud
+
+        xyz = structured_to_unstructured(cloud[['x', 'y', 'z']])
+        traversability = cloud['empty']
+
+        self.scan.set_points(points=xyz)
+        self.scan.set_label(label=traversability)
+
+        depth_img = self.scan.proj_range
+        label = self.scan.proj_sem_label
+
+        return depth_img, label
 
     def __len__(self):
         return len(self.files)
@@ -133,27 +145,50 @@ def images_demo():
         print(len(split))
 
 
-def clouds_demo():
+def clouds_demo(run_times=5):
     from matplotlib import pyplot as plt
+    from traversability_estimation.utils import convert_label
+    import open3d as o3d
+
+    label_map = {255: 0.5,
+                   1: 1.0,
+                   0: 0.0}
 
     path = os.path.join(data_dir, 'bags/traversability/marv/ugv_2022-08-12-16-37-03/')
     ds = TraversabilityClouds(path=path)
 
-    cloud = ds[np.random.choice(len(ds))]
-    xyz = structured_to_unstructured(cloud[['x', 'y', 'z']])
-    intensity = cloud['intensity']
+    for _ in range(run_times):
+        depth_img, label = ds[np.random.choice(len(ds))]
 
-    ds.scan.set_points(points=xyz, remissions=intensity)
+        power = 16
+        depth_img_vis = np.copy(depth_img)  # depth
+        depth_img_vis[depth_img_vis > 0] = depth_img_vis[depth_img_vis > 0] ** (1 / power)
+        depth_img_vis[depth_img_vis > 0] = (depth_img_vis[depth_img_vis > 0] - depth_img_vis[depth_img_vis > 0].min()) / \
+                                           (depth_img_vis[depth_img_vis > 0].max() - depth_img_vis[depth_img_vis > 0].min())
 
-    power = 16
-    depth_img = np.copy(ds.scan.proj_range)  # depth
-    depth_img[depth_img > 0] = depth_img[depth_img > 0] ** (1 / power)
-    depth_img[depth_img > 0] = (depth_img[depth_img > 0] - depth_img[depth_img > 0].min()) / \
-                               (depth_img[depth_img > 0].max() - depth_img[depth_img > 0].min())
+        label_vis = convert_label(label.astype("float"), label_mapping=label_map)
 
-    plt.figure(figsize=(20, 10))
-    plt.imshow(depth_img)
-    plt.show()
+        plt.figure(figsize=(20, 10))
+        plt.subplot(2, 1, 1)
+        # depth_img_vis[depth_img_vis < 0] = 0.5
+        # assert depth_img_vis.min() >= 0.0 and depth_img_vis.max() <= 1.0
+        # plt.imshow((0.3 * depth_img_vis + 0.7 * label_vis).astype("float"))
+        plt.imshow(depth_img_vis)
+        plt.title('Depth image')
+
+        plt.subplot(2, 1, 2)
+        plt.imshow(label_vis)
+        plt.title('Traversable points')
+        plt.tight_layout()
+        plt.show()
+
+        xyz = ds.scan.proj_xyz
+        color = ds.scan.sem_color_lut[label]
+
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(xyz.reshape((-1, 3)))
+        pcd.colors = o3d.utility.Vector3dVector(color.reshape((-1, 3)))
+        o3d.visualization.draw_geometries([pcd])
 
 
 if __name__ == "__main__":
