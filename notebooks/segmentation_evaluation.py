045d51df77adc4dfb1c9b7fb0dba1edc2a7a2e81
Aleš Kučera <kuceral4@fel.cvut.cz>
2022-08-23 14:04:52 +0200

    Change label map in TraversabilityDataset

--- notebooks/segmentation_evaluation.py
+++ notebooks/segmentation_evaluation.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 sys.path.append('/opt/ros/noetic/lib/python3/dist-packages/')
+sys.path.append('/home/ales/catkin_ws/src/traversability_estimation/src')
 # sys.path.append('/home/ales/anaconda3/envs/fiftyone/lib/python3.9/site-packages/')
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
@@ -18,7 +19,6 @@ from PIL import Image
 from hrnet import models
 from hrnet.config import config
 from datasets.traversability_dataset import TraversabilityImages
-from datasets.utils import visualize
 from torch.utils.data import DataLoader
 from datasets.rellis_3d import Rellis3DImages
 from hrnet.core.function import convert_label, convert_color
@@ -28,6 +28,8 @@ from hrnet.core.function import convert_label, convert_color
 pkg_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
 SMP_MODEL = os.path.join(pkg_path, "config/weights/smp/"
                                    "PSPNet_resnext50_32x4d_704x960_lr0.0001_bs6_epoch18_Rellis3D_iou_0.73.pth")
+RESNEXT_MODEL = os.path.join(pkg_path, "config/weights/smp/"
+                                       "fcn_resnet50_lr_1e-05_bs_1_epoch_3_TraversabilityImages_iou_0.71.pth")
 HRNET_MODEL = os.path.join(pkg_path, "config/weights/"
                                      "seg_hrnet_ocr_w48_train_512x1024_sgd_lr1e-2_wd5e-4_bs_12_epoch484/best.pth")
 HRNET_MODEL_CONFIG = os.path.join(pkg_path, "config/hrnet_rellis/"
@@ -42,7 +44,7 @@ class ModelEvaluator(object):
     def __init__(self, model: str, dataset: str, device: str, image_size: tuple):
         """
         Initialize the model evaluator.
-        :param model: hrnet or pspnet
+        :param model: hrnet, pspnet or resnext
         :param dataset: rellis3d or my-dataset
         :param device: cpu or cuda
         :param image_size: (width, height)
@@ -53,23 +55,24 @@ class ModelEvaluator(object):
         self.device = device
 
         # load model
-        assert model in ['hrnet', 'pspnet']
+        assert model in ['hrnet', 'pspnet', 'resnext']
         self.model_name = model
         if model == 'hrnet':
             self.model = self.load_hrnet()
         elif model == 'pspnet':
             self.model = torch.load(SMP_MODEL)
+        elif model == 'resnext':
+            self.model = torch.load(RESNEXT_MODEL)
 
-        # load dataset
-        assert dataset in ['rellis3d', 'my-dataset']
-        self.dataset_name = dataset
-        if dataset == 'rellis3d':
-            self.dataset = Rellis3DImages(crop_size=image_size[::-1], split='test')
-        elif dataset == 'my-dataset':
-            self.dataset = TraversabilityImages(MY_DATASET, MY_DATASET_DIR, image_size[::-1],
-                                                {"train": 0.7, "test": 0.2, "val": 0.1})
         self.split = 'test'
         self.image_size = image_size
+        # load dataset
+        assert dataset in ['Rellis3DImages', 'TraversabilityImages']
+        self.dataset_name = dataset
+        if dataset == 'rellis3d':
+            self.dataset = Rellis3DImages(crop_size=self.image_size[::-1], split=self.split)
+        elif dataset == 'TraversabilityImages':
+            self.dataset = TraversabilityImages(crop_size=self.image_size[::-1], split=self.split)
         self.cfg = yaml.safe_load(open(TRAVERSABILITY_CONFIG, 'r'))
 
     def load_hrnet(self) -> torch.nn.Module:
@@ -128,7 +131,7 @@ class ModelEvaluator(object):
             length = len(self.dataset)
             n = np.random.choice(length)
             image, gt_mask = self.dataset[n]
-        elif self.dataset_name == 'my-dataset':
+        elif self.dataset_name == 'TraversabilityImages':
             length = self.dataset.get_length(self.split)
             n = np.random.choice(length)
             image, gt_mask = self.dataset.get_item(n, self.split)
@@ -149,7 +152,7 @@ class ModelEvaluator(object):
         pred_colormap = convert_color(pr_mask, color_map)
         gt_colormap = convert_color(gt_mask, color_map)
 
-        visualize(image=image_vis, pred=pred_colormap, gt=gt_colormap)
+        # visualize(image=image_vis, pred=pred_colormap, gt=gt_colormap)
 
     def model_predict(self, image: np.ndarray) -> np.ndarray:
         with torch.no_grad():
@@ -160,6 +163,11 @@ class ModelEvaluator(object):
             elif self.model_name == 'hrnet':
                 pred = self.model.forward(image)
                 pred = pred[config.TEST.OUTPUT_INDEX]
+                pred = torch.softmax(pred, dim=1)
+                pred = pred.squeeze().cpu().numpy()
+                mask = np.argmax(pred, axis=0).astype(np.uint8)
+            elif self.model_name == 'resnext':
+                pred = self.model.forward(image)["out"]
                 pred = torch.softmax(pred, dim=1)
                 pred = pred.squeeze().cpu().numpy()
                 mask = np.argmax(pred, axis=0).astype(np.uint8)
@@ -178,93 +186,28 @@ class ModelEvaluator(object):
         return image
 
     def process_ground_truth(self, mask: np.ndarray) -> np.ndarray:
-        if self.dataset_name == 'rellis3D':
-            mask = np.argmax(mask, axis=0).astype(np.uint8) - 1
-            mask = map_labels(mask.squeeze())
-
-        elif self.dataset_name == 'my-dataset':
-            pass
-        else:
-            raise ValueError(f'Unknown dataset: {self.dataset_name}')
+        mask = np.argmax(mask, axis=0).astype(np.uint8)
+        mask = map_labels(mask.squeeze())
         mask = cv2.resize(mask, self.image_size, interpolation=cv2.INTER_LINEAR)
         return mask
 
-    @staticmethod
-    def train_model():
-        ENCODER = 'se_resnext50_32x4d'
-        ENCODER_WEIGHTS = 'imagenet'
-        CLASSES = ['void', 'traversable', 'untraversable']
-        ACTIVATION = 'sigmoid' if len(CLASSES) == 1 else 'softmax2d'
-        DEVICE = 'cuda'
-        IMG_SIZE = (192, 320)
-        LR = 0.0001
-
-        # create segmentation model with pretrained encoder
-        model = smp.FPN(
-            encoder_name=ENCODER,
-            encoder_weights=ENCODER_WEIGHTS,
-            classes=len(CLASSES),
-            activation=ACTIVATION
-        )
-
-        train_dataset = Dataset(classes=CLASSES, crop_size=IMG_SIZE, split='train')
-        valid_dataset = Dataset(classes=CLASSES, crop_size=IMG_SIZE, split='val')
-
-        train_loader = DataLoader(train_dataset, batch_size=3, shuffle=True, num_workers=12)
-        valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=4)
-
-        loss = smp.utils.losses.DiceLoss()
-        metrics = [smp.utils.metrics.IoU(threshold=0.5)]
-        optimizer = torch.optim.Adam([dict(params=model.parameters(), lr=LR)])
-
-        train_epoch = smp.utils.train.TrainEpoch(
-            model,
-            loss=loss,
-            metrics=metrics,
-            optimizer=optimizer,
-            device=DEVICE,
-            verbose=True,
-        )
-
-        valid_epoch = smp.utils.train.ValidEpoch(
-            model,
-            loss=loss,
-            metrics=metrics,
-            device=DEVICE,
-            verbose=True,
-        )
-
-        max_score = 0
-        n_epochs = 3
-        for i in range(0, n_epochs):
-
-            print('\nEpoch: {}'.format(i))
-            train_logs = train_epoch.run(train_loader)
-            valid_logs = valid_epoch.run(valid_loader)
-
-            # do something (save model, change lr, etc.)
-            if max_score < valid_logs['iou_score']:
-                max_score = valid_logs['iou_score']
-                torch.save(model, './best_model.pth')
-                print('Model saved!')
-
-            if i == 25:
-                optimizer.param_groups[0]['lr'] = 1e-5
-                print('Decrease decoder learning rate to 1e-5!')
-
     def save_predictions(self):
-        assert self.dataset_name == 'my-dataset'
-        for i in range(len(self.dataset)):
-            image, gt_mask = self.dataset[i]
+        assert self.dataset_name == 'TraversabilityImages'
+        length = len(self.dataset)
+        train_dataset, valid_dataset = torch.utils.data.random_split(self.dataset,
+                                                                     [int(0.8 * length), int(0.2 * length)],
+                                                                     generator=torch.Generator().manual_seed(42))
+        for i in range(len(valid_dataset)):
+            image, _ = valid_dataset[i]
             x_tensor = self.prepare_image(image)
             mask = self.model_predict(x_tensor)
-            pr_mask = map_labels(mask)
-            self.dataset.save_prediction(pr_mask, i)
+            if self.model != 'resnext':
+                mask = map_labels(mask)
+            self.dataset.save_prediction(mask, i)
 
     def show_dataset(self):
-        assert self.dataset_name == 'my-dataset'
-        session = fo.launch_app(self.dataset.dataset)
-        session.wait()
+        assert self.dataset_name == 'TraversabilityImages'
+        self.dataset.show_dataset()
 
 
 def compute_IoU(pred: np.ndarray, gt: np.ndarray) -> float:
@@ -297,11 +240,11 @@ def map_labels(mask):
 
 
 def main():
-    evaluator = ModelEvaluator('hrnet', 'my-dataset', 'cuda', (320, 192))
+    evaluator = ModelEvaluator('resnext', 'TraversabilityImages', 'cuda', (320, 192))
     # evaluator.evaluate()
-    evaluator.visualize_prediction()
-    # evaluator.save_predictions()
-    # evaluator.show_dataset()
+    # evaluator.visualize_prediction()
+    evaluator.save_predictions()
+    evaluator.show_dataset()
 
 
 if __name__ == '__main__':
